82f66eec10ff2735c8b2720dfeaa290fffef1d85
Amir Dajanovic <amirdajanovic1983@gmail.com>
2025-08-11 22:54:54 +0200

    fix: add RouterLink to standalone components for routerLink binding

--- src/app/components/register-saloon/register-saloon.component.ts
+++ src/app/components/register-saloon/register-saloon.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-saloon',
   templateUrl: './register-saloon.component.html',
-  styleUrls: ['./register-saloon.component.css']
+  styleUrls: ['./register-saloon.component.css'],
+  imports: [RouterLink]
 })
 export class RegisterSaloonComponent {}
--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
+  imports: [RouterLink]
 })
 export class SignInComponent {}
